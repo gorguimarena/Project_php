ce92eebb05ed130f4de9debaba1300acb9acab08
gorguimarena <gorguimarena@gmail.com>
2024-06-26 21:38:33 +0000

    implementation des methode admin

--- models/Administrateur.php
+++ models/Administrateur.php
@@ -25,23 +25,69 @@ class Administrateur
                 return false;
             }
         }else{
-            echo 'error';
+            return false;
         }
 
     }
 
-    public function inscrire_bibliothecaire()
-    {
 
+    public function creerCompte($id,$con,$password,$email)
+    {
+        //on prepare la requete
+        $stmt=$con->prepare("INSERT INTO compte VALUES (?,?,?)");
+        //on donne les parametre de la requete
+        $stmt->bind_param('sss',$email,$password,$id);
+        //on execute la requete
+        $stmt->execute();
+        //on verifie si l'insertio est faite
+        if ($stmt->affected_rows>0){
+            return true;
+        }else{
+            return false;
+        }
     }
 
-    public function modifier_info_user()
+    public function modifier_info_user($id,$nom,$prenom,$status)
     {
-
+        //on recupere la connection
+        $con=database();
+        if ($con){
+            //on prepare la requete
+            $stmt=$con->prepare("UPDATE utilisateur SET (?,?,?) WHERE id_utilisateur=?");
+            //on donne les parametre de la requete
+            $stmt->bind_param('sssi',$prenom,$nom,$status,$id);
+            //on execute la requete
+            $stmt->execute();
+            //on verifie si l'insertio est faite
+            if ($stmt->affected_rows>0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 
-    public function supp_user()
+    public function supp_user($id)
     {
-
+        //on recupere la connection
+        $con=database();
+        if ($con){
+            //on prepare la requete
+            $stmt=$con->prepare("DELETE FROM utilisateur WHERE id_utilisateur=?");
+            //on donne les parametre de la requete
+            $stmt->bind_param('i',$id);
+            //on execute la requete
+            $stmt->execute();
+            //on verifie si l'insertio est faite
+            if ($stmt->affected_rows>0){
+                return true;
+            }else{
+                return false;
+            }
+        }else{
+            return false;
+        }
     }
 }
