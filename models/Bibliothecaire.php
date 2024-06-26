c30e422b2fef8a85f11512c967308675eb44ae5c
hp <mama.sow4@isepdiamniadio.edu.sn>
2024-06-26 16:19:49 +0200

    class implements

---
+++ models/Bibliothecaire.php
@@ -0,0 +1,36 @@
+<?php
+
+class Bibliothecaire{
+    public function ajoutLivre()
+    {
+
+    }
+    public function suppLivre()
+    {
+
+    }
+    public function modifLivre()
+    {
+
+    }
+    public function enregistrerEmprunt()
+    {
+
+    }
+    public function enregistrerRetour()
+    {
+
+    }
+    public function genererRapport()
+    {
+
+    }
+    public function afficherHistoryEmprunt()
+    {
+        
+    }
+
+}
+
+
+?>
