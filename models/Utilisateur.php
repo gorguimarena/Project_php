430fc57ce1758fd1751f7364e4fff061cbd6a7ea
Darkness <rr9891580@gmail.com>
2024-06-29 12:21:42 +0000

    Implementation

--- models/Utilisateur.php
+++ models/Utilisateur.php
@@ -2,22 +2,34 @@
 
 class Utilisateur
 {
-    public function inscrire()
-    {
-        
+    private $nom;
+    private $prenom;
 
-
-
-        
+ public function __construct($nom, $prenom) {
+    $this->nom = $nom;
+    $this->prenom = $prenom;
     }
 
-    public function consulter_livre()
-    {
-        
+
+ public function modifier_info($prenom, $nom) {
+    $this->prenom = $prenom;
+    $this->nom = $nom;
+        return true;
     }
 
-    public function demande_emprunt()
-    {
-
+    
+ public function consulter_info($prenom, $nom) {
+    echo "votre".$this->prenom;
+    echo "votre".$this->nom;
     }
+
+    
+ public function getprenom() {
+    return $this->prenom;
+    }
+
+public function getnom() {
+    return $this->nom;
+    }
+}
 }
