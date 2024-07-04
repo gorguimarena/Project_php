7497f938982af7eabbb3a1b972e5fd3ffe14b4bc
gorguimarena <gorguimarena@gmail.com>
2024-07-04 08:54:13 +0000

    Certaines methodes pour l'utilisateur G

---
+++ models/Utilisateurs.php
@@ -0,0 +1,56 @@
+<?php
+require "../Config/database.php";
+
+class Utilisateurs
+{
+    public function connect($email, $pwd)
+    {
+        $con = database();
+        $smt = $con->prepare("SELECT * FROM compte WHERE email=? AND password=?");
+
+        if ($smt === false) {
+            return false; // Requête incorrecte
+        }
+
+
+        $smt->bind_param("ss", $email, $pwd);
+        $smt->execute();
+        $smt->store_result(); // Nécessaire pour obtenir le nombre de lignes affectées
+
+        if ($smt->num_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function deconnect()
+    {
+        session_start();
+        $_SESSION = [];
+        session_destroy();
+        header("Location: index.php");
+        exit; // Assurez-vous de sortir après la redirection
+    }
+
+    public function sup_compte($id)
+    {
+        $con = database();
+        $smt = $con->prepare("DELETE FROM compte WHERE id=?");
+
+        if ($smt === false) {
+            return false; // Requête incorrecte
+        }
+
+        $smt->bind_param('i', $id);
+        $smt->execute();
+        $smt->store_result(); // Nécessaire pour obtenir le nombre de lignes affectées
+
+        if ($smt->affected_rows > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
