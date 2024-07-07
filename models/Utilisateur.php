14d6a442a6fc76845d3a5bdb183bf25094d87068
hp <mama.sow4@isepdiamniadio.edu.sn>
2024-07-07 14:33:29 +0200

    methode inscrire

--- models/Utilisateur.php
+++ models/Utilisateur.php
@@ -1,48 +1,34 @@
 <?php
-
-class Utilisateur
-{
-    private $nom;
+class Utilisateur {
+    private $id;
     private $prenom;
-
-
-    public function modifier_info($prenom, $nom) {    
-        
-     // Connexion à la base de données
-        $con = database(); if ($con) {
-            // On prépare la requête de mise à jour
-            $stmt = $con->prepare("UPDATE utilisateur SET prenom = ?, nom = ? WHERE id = ?");
-            // On lie les paramètres
-            $stmt->bind_param(" ", $prenom, $nom, $this->id);
-            // On exécuter la requête
-            if ($stmt->execute()) {
-                // Si la mise à jour est réussie, met à jour les propriétés de l'objet
-
-                $this->prenom = $prenom;
-                $this->nom = $nom;
-                return true;
-            } else {
-                // En cas d'erreur
-                return false;
-            }
-        } else {
-            // En cas d'erreur de connexion
+    private $nom;
+    private $email;
+    private $motPasse;
+    private $confMotPasse;
+public function inscrire($prenom, $nom, $email, $motPasse, $confMotPasse) { 
+    // Connexion à la base de données 
+    $con = database(); 
+    if ($con) { 
+        // On préparer la requête d'insertion 
+        $stmt = $con->prepare("INSERT INTO utilisateur (prenom, nom, email, motPasse, confMotPasse) VALUES (?, ?, ?, ?, ?)"); 
+        // Hachage du mot de passe 
+        $hashedPassword = password_hash($motPasse, PASSWORD_BCRYPT); 
+        // Liaison des paramètres 
+        $stmt->bind_param("sssss", $prenom, $nom, $email, $hashedPassword, $confMotPasse); 
+        // Exécution de la requête 
+        if ($stmt->execute()) { 
+            return true;
+         } 
+         else 
+         { 
             return false;
-        }
- }
-    
- public function consulter_info($prenom, $nom) {
-    echo "votre".$this->prenom;
-    echo "votre".$this->nom;
-    }
-
-    
- public function getprenom() {
-    return $this->prenom;
-    }
-
-public function getnom() {
-    return $this->nom;
+         } 
+        } 
+        else 
+        { 
+            return false; 
+        } 
     }
 }
 
